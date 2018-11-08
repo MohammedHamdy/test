c38e987927938e646fdc27956eb1bfdf3874954a
karem90 <karemwork90@gmail.com>
2018-11-08 15:34:10 +0200

    'asd'

--- index2.php
+++ index2.php
@@ -1,9 +1,6 @@
 <?php
-$path = "/xampp/htdocs/test"; 
-$a='';
-chdir($path);
-//exec("git add .");  
-$x = exec("git log --pretty=format:'%h' -n 1");
+exec("git add -A");
+$x = exec("git commit -m 'asd'");
 echo $x;
 echo "<h3 align = center> Succesfully commited all the files.</h3>";
 ?>
