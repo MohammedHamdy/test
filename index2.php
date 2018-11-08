ba2fa0a6756ec49a4c9ab822d8eb9a055d854b2c
karem90 <karemwork90@gmail.com>
2018-11-08 09:36:13 +0200

    'message'

--- index2.php
+++ index2.php
@@ -3,6 +3,7 @@ $path = "/xampp/htdocs/test";
 $a='';
 chdir($path);
 exec("git add .");  
-exec("git commit -m'message'");
+$x = exec("git commit -m'message'");
+echo $x;
 echo "<h3 align = center> Succesfully commited all the files.</h3>";
 ?>
