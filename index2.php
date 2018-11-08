174b0848b1262d6b4717bfdde57640376fb44fac
karem90 <karemwork90@gmail.com>
2018-11-08 09:41:03 +0200

    secons commit

--- index2.php
+++ index2.php
@@ -2,8 +2,8 @@
 $path = "/xampp/htdocs/test"; 
 $a='';
 chdir($path);
-exec("git add .");  
-$x = exec("git commit -m'message'");
+//exec("git add .");  
+$x = exec("git log --pretty=format:'%h' -n 1");
 echo $x;
 echo "<h3 align = center> Succesfully commited all the files.</h3>";
 ?>
